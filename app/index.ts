ed32dbd10feff07c0836f8e5fd5b0cd7693cd97d
ZhengKe <3119531802@qq.com>
2022-06-17 08:41:00 +0800

    fix: clear other

--- app/index.ts
+++ app/index.ts
@@ -8,17 +8,7 @@ import AccessLogMiddleware from "./middleware/AccessLogMiddleware";
 import KoaBody from "koa-body";
 const app = new Koa();
 
-app
-  .use(
-    KoaBody({
-      multipart: true,
-      formidable: {
-        maxFileSize: 200 * 1024 * 1024,
-      },
-    })
-  )
-  .use(AccessLogMiddleware)
-  .use(router.routes());
+app.use(KoaBody()).use(AccessLogMiddleware).use(router.routes());
 
 const run = (port: any): Server => {
   return app.listen(port);
